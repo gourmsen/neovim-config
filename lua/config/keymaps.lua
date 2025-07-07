c88ec31a2995a6c5c44f2d84cfabc635318d240a
Maximilian Niessl <mniessl@horizont-it.com>
2025-07-07 16:49:03 +0200

    Add diagnostics and prettier

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,14 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>f", builtin.find_files)
 vim.keymap.set("n", "<leader>b", builtin.buffers)
+vim.keymap.set("n", "<leader>d", builtin.diagnostics)
 
 -- tree
 vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
+
+-- prettier
+vim.keymap.set("n", "<leader>p", function()
+    local pos = vim.api.nvim_win_get_cursor(0)
+    vim.cmd("silent %!npx prettier --stdin-filepath %")
+    pcall(vim.api.nvim_win_set_cursor, 0, pos)
+end)
