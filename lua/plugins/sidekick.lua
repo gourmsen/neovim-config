d64a8690a53e7b09395938aa0ff9a70088073d47
Maximilian Niessl <mniessl@horizont-it.com>
2025-11-19 14:31:00 +0100

    Add sidekick

---
+++ lua/plugins/sidekick.lua
@@ -0,0 +1,40 @@
+return {
+    "folke/sidekick.nvim",
+    opts = {
+        cli = {
+            nes = {
+                enable = false,
+            },
+            win = {
+                layout = "right",
+                split = {
+                    width = 80,
+                    height = 20,
+                }
+            },
+            mux = {
+                enabled = true,
+                backend = "tmux",
+            },
+        }
+    },
+    keys = {
+        {
+            "<leader>ai",
+            function() require("sidekick.cli").toggle({ name = "copilot", focus = true }) end,
+            desc = "Sidekick Toggle Copilot",
+        },
+        {
+            "<leader>at",
+            function() require("sidekick.cli").send({ name = "copilot", focus = true, msg = "{this}" }) end,
+            mode = { "x", "n" },
+            desc = "Send This",
+        },
+        {
+            "<leader>ad",
+            function() require("sidekick.cli").send({ name = "copilot", focus = true, msg = "{diagnostics}" }) end,
+            mode = { "x", "n" },
+            desc = "Send Diagnostics",
+        },
+    },
+}
